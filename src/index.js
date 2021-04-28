eb9d42fc678cece8a7966ba8ce5d74e774c6e926
letsdothispej <pauljamesazanes@gmail.com>
2021-04-28 20:10:59 +0800

    functions done

--- src/index.js
+++ src/index.js
@@ -1,19 +1,42 @@
 const generateNumberHeading = document.querySelector(".generateNumberHeading"),
-    generateNumberForm = document.querySelector(".generateNumberForm")
-    generateNumberRange = generateNumberForm.querySelector("input");
+  generateNumberRange = document.querySelector(".generateNumberRange"),
+  guessNumberInput = document.querySelector(".guessNumberInput"),
+  playBtn = document.querySelector("button"),
+  resultContainer = document.querySelector(".resultContainer"),
+  numberResult = resultContainer.querySelector(".numberResult"),
+  gameResult = resultContainer.querySelector(".gameResult"),
+  inputError = document.querySelector(".inputError");
 
-function paintNumberRange(){
-    const currentValue = generateNumberRange.value;
-    generateNumberHeading.innerText = `Guess the Number between 0 and ${currentValue}`;
+let rangeValue = generateNumberRange.value;
+
+function handleClick() {
+  if (guessNumberInput.value !== "") {
+    inputError.classList.remove("showing");
+    resultContainer.classList.add("showing");
+    const inputValue = guessNumberInput.value;
+    const randomValue = Math.floor(Math.random() * rangeValue + 1);
+    numberResult.innerText = `You chose ${inputValue}, the machine chose ${randomValue}`;
+    gameResult.innerText =
+      Number(inputValue) === randomValue ? "You win!" : "You lose!";
+  } else {
+      inputError.classList.add("showing");
+  }
 }
 
-function handleOnInput(event){
-    generateNumberHeading.innerText = `Guess the Number between 0 and ${this.value}`;
+function paintNumberRange() {
+  generateNumberHeading.innerText = `Guess the Number between 0 and ${rangeValue}`;
+  guessNumberInput.max = rangeValue;
 }
 
-function init(){
-    paintNumberRange();
-    generateNumberRange.oninput = handleOnInput;
+function handleOnInput() {
+  rangeValue = this.value;
+  paintNumberRange();
+}
+
+function init() {
+  paintNumberRange();
+  generateNumberRange.oninput = handleOnInput;
+  playBtn.addEventListener("click", handleClick);
 }
 
 init();
